73c09ec592d3918e3cfb3a6f49228e5a18f99a14
1Alex4949031 <a.seleznev@g.nsu.ru>
2022-09-15 09:40:16 +0700

    javadoc

--- Task_1_1_1/src/main/java/HeapSort.java
+++ Task_1_1_1/src/main/java/HeapSort.java
@@ -1,24 +1,27 @@
 /**
- * Heapsort class.
+ * HeapSort class.
+ *
+ * @author Алексей Селезнев
  */
 public class HeapSort {
+  /**
+   * Making default swap function
+   * a -> b, b -> a
+   */
   public static void swap(int[] arr, int a, int b) {
-    /**
-     * Making default swap function
-     * a -> b, b -> a
-     **/
+    /** making buffer for changing the value of array*/
     int buff;
     buff = arr[a];
     arr[a] = arr[b];
     arr[b] = buff;
   }
 
+  /**
+   * Finding max-heap.
+   * Checking the sons of the current root,
+   * trying to the biggest one.
+   */
   public static void heap(int[] arr, int len, int i) {
-    /**
-     * Finding max-heap.
-     * Checking the sons of the current root,
-     * trying to the biggest one.
-     */
     int largest = i;
     int r = i * 2 + 2;
     int l = i * 2 + 1;
@@ -35,14 +38,15 @@ public class HeapSort {
     }
   }
 
+  /**
+   * 1. Getting the max-heap tree.
+   * 2. Changing the root and the last element of the heap.
+   * Then make the heap smaller by decreasing by 1. Getting
+   * the max-heap with the new root.
+   * 3. Start with the 1 sentence while size of heap > 1.
+   */
   public static void heapsort(int[] arr) {
-    /**
-     * 1. Getting the max-heap tree.
-     * 2. Changing the root and the last element of the heap.
-     * Then make the heap smaller by decreasing by 1. Getting
-     * the max-heap with the new root.
-     * 3. Start with the 1 sentence while size of heap > 1.
-     */
+    /** the length of the array*/
     int len = arr.length;
 
     for (int i = len / 2 - 1; i >= 0; i--) {
@@ -54,10 +58,9 @@ public class HeapSort {
     }
   }
 
+  /** Main made for first test */
   public static void main(String[] args) {
-    /**
-     * Test example.
-     */
+    /** Test array */
     int[] arr = {3, 5, 1, 10, 12};
     int len = arr.length;
 
