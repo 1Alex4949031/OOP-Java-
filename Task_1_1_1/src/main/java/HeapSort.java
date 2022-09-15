b7ea98111763287cea98a7fbb6edb54654a17006
1Alex4949031 <a.seleznev@g.nsu.ru>
2022-09-15 10:42:17 +0700

    i m tired

--- Task_1_1_1/src/main/java/HeapSort.java
+++ Task_1_1_1/src/main/java/HeapSort.java
@@ -67,26 +67,4 @@ public class HeapSort {
       heap(arr, i, 0);
     }
   }
-
-  /**
-   * Main made for the first test.
-   *
-   * @param args string arguments
-   */
-
-  public static void main(String[] args) {
-    //int[] arr = {3, 5, 1, 10, 12};
-    //int len = arr.length;
-    //heapsort(arr);
-
-    System.out.print("{");
-    for (int i = 0; i < len; i++) {
-      if (i != len - 1) {
-        System.out.print(arr[i] + ",");
-      } else {
-        System.out.print(arr[i]);
-      }
-    }
-    System.out.print("}");
-  }
 }
