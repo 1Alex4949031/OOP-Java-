b4d064094a6724dacf1035ece7e16ff11c891e44
1Alex4949031 <1Alex4949031@users.noreply.github.com>
2022-09-26 12:35:46 +0000

    Deploying to gh-pages from @ 1Alex4949031/OOP-Java-@b8bd8092c9a7be14aa7ecd1ac57a99477cf8dfbb 🚀

--- Task_1_2_1/type-search-index.js
+++ Task_1_2_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Stack"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.a.seleznev","l":"Stack"}]
