5ea7271643e46683327ef7a2c561fdbefbb27489
1Alex4949031 <a.seleznev@g.nsu.ru>
2022-09-26 19:26:47 +0700

    fix

--- Task_1_2_1/script.bat
+++
@@ -1,8 +0,0 @@
-javadoc -d .\make\ -sourcepath .\src\main\java -subpackages ru.nsu.a.seleznev
-
-javac -d .\make\bin\ -sourcepath .\src\main\java .\src\main\java\ru\nsu\a\seleznev\Stack.java
-
-mkdir .\make\jar
-jar cf .\make\jar\Stack.jar -C .\make\bin .
-
-java -classpath .\make\jar\Stack.jar ru.nsu.a.seleznev.Stack
