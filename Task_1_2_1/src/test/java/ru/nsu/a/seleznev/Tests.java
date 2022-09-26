78451d10980c951417f54b22531d7e633ba73ef2
1Alex4949031 <a.seleznev@g.nsu.ru>
2022-09-26 19:41:16 +0700

    4-2-revdoc

--- Task_1_2_1/src/test/java/ru/nsu/a/seleznev/Tests.java
+++ Task_1_2_1/src/test/java/ru/nsu/a/seleznev/Tests.java
@@ -7,85 +7,85 @@ import org.junit.jupiter.api.Test;
  * Tests implementation for check the correctness of stack.
  */
 class Tests {
-    /**
-     * Test number 1 for Integer type.
-     */
-    @Test
-    public void theFirstTest() {
-        Stack<Integer> t1 = new Stack<>(2);
-        t1.push(10);
-        t1.push(20);
-        t1.push(30);
-        t1.push(40);
-        t1.popStack(2);
+  /**
+   * Test number 1 for Integer type.
+   */
+  @Test
+  public void theFirstTest() {
+    Stack<Integer> t1 = new Stack<>(2);
+    t1.push(10);
+    t1.push(20);
+    t1.push(30);
+    t1.push(40);
+    t1.popStack(2);
 
-        Stack<Integer> test = new Stack<>(2);
-        test.push(77);
-        test.push(66);
-        t1.pushStack(test);
+    Stack<Integer> test = new Stack<>(2);
+    test.push(77);
+    test.push(66);
+    t1.pushStack(test);
 
-        Stack<Integer> act = new Stack<>(4);
-        act.push(10);
-        act.push(20);
-        act.push(77);
-        act.push(66);
+    Stack<Integer> act = new Stack<>(4);
+    act.push(10);
+    act.push(20);
+    act.push(77);
+    act.push(66);
 
 
-        Assertions.assertEquals(t1.count(), act.count());
-        Assertions.assertArrayEquals(t1.toArray(), act.toArray());
-        Assertions.assertEquals(t1, act);
-    }
+    Assertions.assertEquals(t1.count(), act.count());
+    Assertions.assertArrayEquals(t1.toArray(), act.toArray());
+    Assertions.assertEquals(t1, act);
+  }
 
-    /**
-     * Test number 2 for String type.
-     */
-    @Test
-    public void theSecondTest() {
-        Stack<String> t2 = new Stack<>(3);
-        t2.push("Hello");
-        t2.push("How are you?");
-        t2.push("Knave");
+  /**
+   * Test number 2 for String type.
+   */
+  @Test
+  public void theSecondTest() {
+    Stack<String> t2 = new Stack<>(3);
+    t2.push("Hello");
+    t2.push("How are you?");
+    t2.push("Knave");
 
-        Stack<String> pu = new Stack<>(3);
-        pu.push("Hello");
-        pu.push("How are you?");
-        pu.push("Friend");
+    Stack<String> pu = new Stack<>(3);
+    pu.push("Hello");
+    pu.push("How are you?");
+    pu.push("Friend");
 
-        t2.pop();
-        t2.push("Friend");
+    t2.pop();
+    t2.push("Friend");
 
-        Assertions.assertEquals(t2.count(), pu.count());
-        Assertions.assertArrayEquals(t2.toArray(), pu.toArray());
-        Assertions.assertEquals(t2, pu);
-    }
+    Assertions.assertEquals(t2.count(), pu.count());
+    Assertions.assertArrayEquals(t2.toArray(), pu.toArray());
+    Assertions.assertEquals(t2, pu);
+  }
 
-    /**
-     * Test number 3 for Double type.
-     */
-    @Test
-    public void theThirdTest() {
-        Stack<Double> t3 = new Stack<>(10);
-        t3.push(1.03);
+  /**
+   * Test number 3 for Double type.
+   */
+  @Test
+  public void theThirdTest() {
+    Stack<Double> t3 = new Stack<>(10);
+    t3.push(1.03);
 
-        Stack<Double> st = new Stack<>(6);
-        st.push(1.04);
-        st.push(1.05);
-        st.push(1.06);
-        st.push(2.22);
-        st.push(3.33);
-        st.push(4.44);
+    Stack<Double> st = new Stack<>(6);
+    st.push(1.04);
+    st.push(1.05);
+    st.push(1.06);
+    st.push(2.22);
+    st.push(3.33);
+    st.push(4.44);
 
-        t3.pushStack(st);
-        t3.popStack(3);
+    t3.pushStack(st);
+    t3.popStack(3);
 
-        Stack<Double> act = new Stack<>(10);
-        act.push(1.03);
-        act.push(1.04);
-        act.push(1.05);
-        act.push(1.06);
+    Stack<Double> act = new Stack<>(10);
+    act.push(1.03);
+    act.push(1.04);
+    act.push(1.05);
+    act.push(1.06);
 
-        Assertions.assertEquals(t3.count(), act.count());
-        Assertions.assertArrayEquals(t3.toArray(), act.toArray());
-        Assertions.assertEquals(t3, act);
-    }
+    Assertions.assertEquals(t3.count(), act.count());
+    Assertions.assertArrayEquals(t3.toArray(), act.toArray());
+    Assertions.assertEquals(t3, act);
+  }
 }
