5364f2a4a27024021ea19bcde15805448b603203
1Alex4949031 <a.seleznev@g.nsu.ru>
2022-09-28 15:43:32 +0700

    fix

--- Task_1_1_1/script.bat
+++
@@ -1,8 +0,0 @@
-javadoc -d .\make\ -sourcepath .\src\main\java -subpackages ru.nsu.a.seleznev
-
-javac -d .\make\bin\ -sourcepath .\src\main\java .\src\main\java\ru\nsu\a\seleznev\HeapSort.java
-
-mkdir .\make\jar
-jar cf .\make\jar\HeapSort.jar -C .\make\bin .
-
-java -classpath .\make\jar\HeapSort.jar ru.nsu.a.seleznev.HeapSort
