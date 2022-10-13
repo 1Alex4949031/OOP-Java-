22df73b41963008418aa478d6f0b7f80e71efc46
1Alex4949031 <a.seleznev@g.nsu.ru>
2022-10-13 17:24:51 +0700

    Fix review doc

--- Task_1_2_2/src/test/java/ru/nsu/a/seleznev/TreeTests.java
+++ Task_1_2_2/src/test/java/ru/nsu/a/seleznev/TreeTests.java
@@ -1,7 +1,7 @@
 package ru.nsu.a.seleznev;
 
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
