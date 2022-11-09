3a11af320281b2392d4f97f21d5568500f371f2e
1Alex4949031 <91533963+1Alex4949031@users.noreply.github.com>
2022-11-09 13:54:03 +0700

    Update GraphInputTests.java

--- Task_1_2_3/src/test/java/ru/nsu/seleznev/a/GraphInputTests.java
+++ Task_1_2_3/src/test/java/ru/nsu/seleznev/a/GraphInputTests.java
@@ -44,8 +44,6 @@ public class GraphInputTests {
         }
       }
 
-      file.close();
-
       Map<String, Integer> act;
       Graph<String> graph = new Graph<>(vertexes, list, weights, edgesArr);
       act = graph.dijkstra("d", verCount);
@@ -87,8 +85,6 @@ public class GraphInputTests {
           matrix[i][j] = scan.nextInt();
         }
       }
-
-      file.close();
 
       Graph<String> graph = new Graph<>(matrix, vertexes);
       Map<String, Integer> act;
@@ -135,8 +131,6 @@ public class GraphInputTests {
       for (int i = 0; i < edgesCount; i++) {
         matrix[verCount][i] = scan.nextInt();
       }
-
-      file.close();
 
       Graph<Integer> graph = new Graph<>(matrix, vertexes, edgesCount);
       Map<Integer, Integer> act;
