db0f4cd82a8b107d1e06ddfc5f1da14e45bbc4f1
1Alex4949031 <1Alex4949031@users.noreply.github.com>
2022-12-14 12:53:54 +0000

    Deploying to gh-pages from @ 1Alex4949031/OOP-Java-@3d4d0878d74cfbbd674a76f6b9a90b4ed282a3f0 🚀

---
+++ Task_1_5_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.seleznev.a","l":"Calculator"},{"p":"ru.nsu.seleznev.a","l":"ConsoleCalculator"}];updateSearchResults();
