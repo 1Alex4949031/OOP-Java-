14547a6169ebdd8f43851b2be1ff9ef98797d39c
1Alex4949031 <1Alex4949031@users.noreply.github.com>
2022-12-21 05:13:20 +0000

    Deploying to gh-pages from @ 1Alex4949031/OOP-Java-@0a6cdde99115e9b3ee95dfd86c65fee6e875c15f 🚀

---
+++ Task_1_5_2/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"ru.nsu.seleznev.a","c":"NoteBook","l":"baseFile"},{"p":"ru.nsu.seleznev.a","c":"NoteBook","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"ru.nsu.seleznev.a","c":"NoteBook","l":"NoteBook()","u":"%3Cinit%3E()"}];updateSearchResults();
