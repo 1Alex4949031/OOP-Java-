0878251702bb981d5eb7f614009191a3d6a3d6d7
1Alex4949031 <a.seleznev@g.nsu.ru>
2023-04-25 14:34:08 +0700

    Add empty Test

---
+++ Task_2_3_1/src/test/java/ru/nsu/seleznev/a/task_2_3_1/EmptyTest.java
@@ -0,0 +1,12 @@
+package ru.nsu.seleznev.a.task_2_3_1;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EmptyTest {
+  @Test
+  public void emptyTest() {
+    Assertions.assertEquals(2 + 2, 4);
+  }
+}
